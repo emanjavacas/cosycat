9ea3b35d1cdbb171a1524951d1888977f775cd1c
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-09-05 17:00:12 +0200

    simplify minimize btn panels

--- src/cljs/cleebo/query/components/minimize_panel.cljs
+++ src/cljs/cleebo/query/components/minimize_panel.cljs
@@ -5,30 +5,28 @@
 
 (defn default-header [] [:div ""])
 
-(defn minimize-panel-header [open-header closed-header {:keys [id dir open]}]
-  (fn [open-header closed-header {:keys [id dir open]}]
+(defn minimize-panel-header [open-header closed-header {:keys [id open]}]
+  (fn [open-header closed-header {:keys [id open]}]
     [:div.container-fluid
      [:div.row
       [:span.pull-right
        [bs/button-toolbar
-        [bs/button
-         {:onClick #(re-frame/dispatch [:panel-order id dir]) :bsSize "xsmall"}
-         [bs/glyphicon {:glyph (if (= :bottom dir) "chevron-down" "chevron-up")}]]
-        [bs/button
-         {:onClick #(re-frame/dispatch [:panel-open id (not @open)]) :bsSize "xsmall"}
-         [bs/glyphicon {:glyph (if @open "collapse-up" "collapse-down")}]]]]
+        (let [other-panel (if (= id "query-frame") "annotation-panel" "query-frame")
+              dir (if @open :bottom :top)]
+          [bs/button {:onClick #(do (re-frame/dispatch [:panel-order id dir])
+                                    (re-frame/dispatch [:panel-open id (not @open)])
+                                    (re-frame/dispatch [:panel-open other-panel @open]))}
+           [bs/glyphicon {:glyph "sort"}]])]]
       (if @open [open-header] [closed-header])]]))
 
 (defn minimize-panel
   [{:keys [child args id init open-header closed-header]
     :or {init true open-header default-header close-header default-header}}]
-  (let [open (re-frame/subscribe [:project-session :components :panel-open id])
-        panel-order (re-frame/subscribe [:project-session :components :panel-order])]
+  (let [open (re-frame/subscribe [:project-session :components :panel-open id])]
     (fn [{:keys [child init args]}]
-      (let [dir (if (= id (first @panel-order)) :bottom :top)]
-        [bs/panel
-         {:collapsible true
-          :expanded @open
-          :header (reagent/as-component
-                   [minimize-panel-header open-header closed-header {:id id :dir dir :open open}])}
-         (into [child] args)]))))
+      [bs/panel
+       {:collapsible true
+        :expanded @open
+        :header (reagent/as-component
+                 [minimize-panel-header open-header closed-header {:id id :open open}])}
+       (into [child] args)])))
