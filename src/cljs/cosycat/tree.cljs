7a9fade977542833c17f04d6a7df1891a572c9e4
emanjavacas <enrique.manjavacas@gmail.com>
2016-09-13 13:04:42 +0200

    improved data-tree

--- src/cljs/cosycat/tree.cljs
+++ src/cljs/cosycat/tree.cljs
@@ -1,33 +1,60 @@
 (ns cosycat.tree
   (:require [reagent.core :as reagent]))
 
-(defn style [depth] {:margin-left (str (+ 10 depth) "px") :cursor "pointer"})
+(def label-style
+  {:padding "2px 4px"
+   :font-size "80%"
+   :color "#c7254e"
+   :margin-bottom "0px"
+   :background-color "#f9f2f4"
+   :font-family "Menlo,Monaco,Consolas,\"Courier New\",monospace"
+   :border-radius "3px"})
 
-(defn i [k v depth children & {:keys [tag] :or {tag :div}}]
-  (let [open (reagent/atom true)]
+(def nil-label-style
+  (merge label-style {:color "1DC48D" :background-color "B4E7D6"}))
+
+(defn child-label [data]
+  (cond
+    (nil? data)   [:label {:style nil-label-style} "null"]
+    (empty? (str data)) [:label {:style label-style} "\"\""]
+    :else [:label {:style label-style} (str data)]))
+
+(defn is-last-child [children]
+  (= :span (first children)))
+
+(defn style [depth] {:margin-left (str (+ 10 depth) "px")})
+
+(defn i [k v depth children & {:keys [tag init-open] :or {tag :div init-open true}}]
+  (let [open (reagent/atom init-open)]
     (fn [k v depth children & {:keys [tag] :or {tag :div}}]
-      [tag
-       {:style (merge (style depth))}
+      [tag {:style (merge (style depth))}
        [:span
-        [:span {:on-click #(do (.stopPropagation %)  (swap! open not))}
-         [:i.glyphicon
-          {:class (if @open "glyphicon-triangle-bottom" "glyphicon-triangle-right")}]]
+        (if-not (is-last-child children)
+          [:i.glyphicon
+           {:class (if @open "glyphicon-triangle-bottom" "glyphicon-triangle-right")
+            :on-click #(do (.stopPropagation %)  (swap! open not))
+            :style {:cursor "pointer"}}]
+          [:i.glyphicon
+           {:class "glyphicon-chevron-right"
+            :style {:margin-left "-1px" :padding-right "2px"}}])
         (str k)]
-       [:div {:style {:margin-left "15px"}} (when @open children)]])))
+       [:span {:style {:margin-left "15px"}} (when (or @open (is-last-child children)) children)]])))
 
-(defn recursive* [data depth]
+(defn recursive* [data depth init-open]
   (cond (map? data)                     ;map
         (into [:div {:style (style depth)}]
-              (mapv (fn [[k v]] [i k v depth (recursive* v (inc depth)) :tag :div])
+              (mapv (fn [[k v]] [i k v depth (recursive* v (inc depth))
+                                 :tag :div :init-open init-open])
                     data))
-        (sequential? data)              ;sequential
-        (into [:ul {:style (style depth) :list-style-type "none"}]
-              (mapv (fn [[k v]] [i k v depth (recursive* v (inc depth)) :tag :li])
+        (coll? data)              ;sequential
+        (into [:ul {:style (assoc (style depth) :list-style-type "none")}]
+              (mapv (fn [[k v]] [i k v depth (recursive* v (inc depth))
+                                 :tag :li :init-open init-open])
                     (map-indexed vector data)))
-        :else (str data)))              ;else
+        :else [:span [child-label data]]))              ;else
 
-(defn recursive [data] [recursive* data 0])
+(defn recursive [data init-open] [recursive* data 0 init-open])
 
-(defn data-tree [data]
+(defn data-tree [data & {:keys [init-open] :or {init-open true}}]
   (fn [data]
-    [recursive data]))
+    [recursive data init-open]))
