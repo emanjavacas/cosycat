1b2e8d144741960bdeb4ec2d99f3eb299d346fb9
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-09-27 14:35:06 +0200

    added project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cosycat "0.1.0-SNAPSHOT"
+(defproject cosycat "0.1.0-alpha"
   :description "Corpus query interface plus annotations"
   :license {:name "GNU v3.0"}  
   :dependencies [[org.clojure/clojure "1.8.0"]
