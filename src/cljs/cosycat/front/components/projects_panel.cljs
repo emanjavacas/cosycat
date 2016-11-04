82aa49611bb8d88609b10cf4ee2e9996213797fd
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-11-04 13:34:30 +0100

    fixed overflow

--- src/cljs/cosycat/front/components/projects_panel.cljs
+++ src/cljs/cosycat/front/components/projects_panel.cljs
@@ -11,18 +11,22 @@
 (defn user-cell [{username :username}]
   (let [user-info (re-frame/subscribe [:user username])]
     (fn [user]
-      [:td
-       {:style {:padding-right "10px"}}
-       [bs/overlay-trigger
-        {:overlay (reagent/as-component [bs/tooltip {:id "tooltip"} username])
-         :placement "bottom"
-         :class "pull-right"}
-        [user-selection-component @user-info]]])))
+      [bs/overlay-trigger
+       {:overlay (reagent/as-component [bs/tooltip {:id "tooltip"} username])
+        :placement "bottom"
+        :class "pull-right"}
+       [user-selection-component @user-info]])))
 
 (defn users-row [creator users]
   (fn [creator users]
-    [:table [:tbody [:tr (doall (for [{username :username :as user} users]
-                                  ^{:key username} [user-cell user]))]]]))
+    [:table
+     [:tbody
+      [:tr
+       (doall (for [{username :username :as user} users]
+                ^{:key username}
+                [:td
+                 {:style {:padding "8px 8px 8px 0px !important"}}
+                 [user-cell user]]))]]]))
 
 (defn spacer [& {:keys [height] :or {height 5}}]
   [:div.row {:style {:height (str height "px")}}])
@@ -39,19 +43,21 @@
            [:div description]]]
          [spacer :height 20]
          [:div.row
-          [:div.col-lg-3 [:span.text-muted "Created by: "]]
-          [:div.col-lg-9 [user-selection-component @creator-info]]]
+          [:div.col-lg-3.col-md-3.col-sm-3 [:span.text-muted "Created by: "]]
+          [:div.col-lg-9.col-md-9.col-sm-9 [user-selection-component @creator-info]]]
+         [spacer]         
+         [:div.row
+          [:div.col-lg-3.col-md-3.col-sm-3 [:span.text-muted "Created on: "]]
+          [:div.col-lg-9.col-md-9.col-sm-9 [:span (human-time created)]]]
          [spacer]
          [:div.row
-          [:div.col-lg-3 [:span.text-muted "Users in project: "]]
-          [:div.col-lg-9 [users-row creator users]]]
-         [spacer :height 10]
-         [:div.row
-          [:div.col-lg-3 [:span.text-muted "Created on: "]]
-          [:div.col-lg-9 [:span (human-time created)]]]
+          [:div.col-lg-3.col-md-3.col-sm-3 [:span.text-muted "Users in project: "]]
+          [:div.col-lg-9.col-md-9.col-sm-9
+           {:style {:overflow-x "auto"}}
+           [users-row creator users]]]
          [spacer]])])))
 
-(defn projects-panel* [projects]
+(defn projects-subpanel [projects]
   (fn [projects]
     [:div.container-fluid
      [:div.row
@@ -67,4 +73,4 @@
     (fn []
       (if (zero? (count @projects))
         [:div [no-projects]]
-        [:div [projects-panel* projects]]))))
+        [:div [projects-subpanel projects]]))))
