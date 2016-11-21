b785068679445ce411f0d1ea8a2a4417c8a113f6
emanjavacas <enrique.manjavacas@gmail.com>
2016-11-21 20:27:20 +0100

    done remove project issue

--- src/cljs/cosycat/project/components/issues/remove_project_component.cljs
+++ src/cljs/cosycat/project/components/issues/remove_project_component.cljs
@@ -2,33 +2,61 @@
   (:require [reagent.core :as reagent]
             [re-frame.core :as re-frame]
             [react-bootstrap.components :as bs]
+            [clojure.string :refer [capitalize]]
             [cosycat.utils :refer [format]]
             [cosycat.project.components.issues.issue-thread-component
              :refer [issue-thread-component]]
             [taoensso.timbre :as timbre]))
 
-(defn remove-message [{{:keys [agreed]} :data}]
-  (let [[one & others] agreed]
-    (if-not others
-      (format "%s wants to remove the project." one)
-      (format "%s and %s want to remove the project." (apply str (interpose ", " others)) one))))
+(defn remove-message [{{:keys [agreed]} :data issue-id :id :as issue} me]
+  (let [[one & others] agreed
+        one (if (= one me) "you" one)
+        others (map #(if (= % me) "you" %) others)]
+    (if (empty? others)
+      (if (= one "you")
+        [:span [:strong "You"] " want to remove the project."]
+        [:span [:strong one] " wants to remove the project."])
+      (let [sep (if (< 2 (count others)) ", " "")]
+        [:span (doall (for [other (cons (capitalize (first others)) (rest others))]
+                        ^{:key (str other "-" issue-id)}
+                        [:span [:strong other] sep]))
+         " and "
+         [:strong one]
+         " want to remove the project."]))))
 
-(defn pending-message [{{:keys [agreed]} :data} project-users]
-  (let [[one & others] (remove (apply hash-set agreed) project-users)]
-    (if-not others
-      (format "%s is pending to decide on this issue." one)
-      (format "%s and %s are pending to decide on this issue."
-              (apply (str (interpose ", others")) one)))))
+(defn you-pending [& {:keys [capitalize?] :or {capitalize? false}}]
+  [:span {:style {:cursor "pointer"}}
+   [:a {:onClick #(re-frame/dispatch [:open-modal :delete-project])}
+    [:strong (if capitalize? "You" "you")]]])
+
+(defn pending-message [{{:keys [agreed]} :data issue-id :id :as issue} project-users me]
+  (let [[one & others] (remove (apply hash-set agreed) project-users)
+        one (if (= one me) "you" one)
+        others (map #(if (= % me) "you" %) others)]
+    (if (empty? others)
+      (if (= one "you")
+        [:span [you-pending :capitalize? true] " are pending to decide on this issue."]
+        [:span [:strong one] " is pending to decide on this issue."])
+      (let [sep (if (< 2 (count others)) ", " "")]
+        [:span (doall (for [other (cons (capitalize (first others)) (rest others))]
+                        ^{:key (str other "-" issue-id)}
+                        [:span (condp other
+                                   "You" [you-pending :capitalize? true]
+                                   "you" [you-pending]
+                                   :else [:strong other])
+                         sep]))
+         " and "
+         (if (= one "you") [you-pending] [:strong one])
+         " are pending to decide on this issue."]))))
 
 (defn remove-project-component [issue]
   (let [users (re-frame/subscribe [:active-project :users])
         me (re-frame/subscribe [:me :username])]
     (fn [{{:keys [agreed]} :data :as issue}]
-      (let [me-pending? (not (some #{@me} agreed))
-            rem-msg (remove-message issue)
-            pen-msg (pending-message issue (map :username @users))]
-        [:div.container-fluid
-         [:div.row [:p rem-msg] [:p pen-msg]]
-         [:div.row {:style {:height "10px"}}]
-         [:div.row [issue-thread-component issue]]]))))
+      [:div.container-fluid
+       [:div.row
+        [:p [remove-message issue @me]]
+        [:p [pending-message issue (map :username @users) @me]]]
+       [:div.row {:style {:height "10px"}}]
+       [:div.row [issue-thread-component issue]]])))
 
