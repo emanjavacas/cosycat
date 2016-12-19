6ad0d54094746c3e5d5baa39a06afe3d95727034
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-12-19 16:31:22 +0100

    new minor version (includes reviews and other bug fixes)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cosycat "0.1.5-alpha"
+(defproject cosycat "0.2.0-alpha"
   :description "Corpus query interface plus annotations"
   :license {:name "GNU v3.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
