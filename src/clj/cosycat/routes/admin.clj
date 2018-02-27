c3b1d5695a00c3201a6e26f4226c15a7a24ca1eb
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-02-27 14:16:56 +0100

    added admin/remove-project backend route

--- src/clj/cosycat/routes/admin.clj
+++ src/clj/cosycat/routes/admin.clj
@@ -4,8 +4,9 @@
             [compojure.core :refer [routes context POST GET]]
             [cosycat.app-utils :refer [->int]]
             [cosycat.routes.auth :refer [is-admin?]]
-            [cosycat.routes.utils
-             :refer [make-default-route ex-user check-user-rights]]
+            [cosycat.routes.utils :refer [make-default-route ex-user check-user-rights]]
+            [cosycat.db.projects :as proj]
+            [cosycat.components.ws :refer [send-clients send-client]]
             [config.core :refer [env]]
             [taoensso.timbre :as timbre]))
 
@@ -44,7 +45,20 @@
   [{{from :from to :to :or {to (+ (->int from) 10)}} :params}]
   {:lines (vec (lines-in-range (lazy-log-lines (:log-dir env)) (->int from) (->int to)))})
 
+(defn remove-project-route
+  [{{{username :username} :identity} :session
+    {db :db ws :ws} :components
+    {project-name :project-name} :params}]
+  (if-let [{:keys [users]} (proj/find-project-by-name db project-name)]
+    (do (proj/erase-project db project-name)
+        (send-clients ws {:type :remove-project :data {:project-name project-name}}
+                      :source-client username
+                      :target-clients (mapv :username users)))))
+
 (defn admin-routes []
   (routes
    (context "/admin" []
-    (GET "/log" [] (make-default-route log-route :is-ok? is-admin?)))))
+     (GET "/log" [] (make-default-route log-route :is-ok? is-admin?))
+     (POST "/remove-project" []
+           (make-default-route remove-project-route :is-ok? is-admin?)))))
+
